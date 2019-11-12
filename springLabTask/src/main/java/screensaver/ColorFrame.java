0df45ba9b2909d69ff9163a5c4e4bcefbaac999e
AleksandraBogdan <aleksandrabogdan@mail.ru>
2019-11-12 21:36:02 +0300

    add ColorFrame and periodical scope

---
+++ springLabTask/src/main/java/screensaver/ColorFrame.java
@@ -0,0 +1,25 @@
+package screensaver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
+public abstract class ColorFrame extends JFrame {
+
+    public ColorFrame() {
+        setSize(200, 200);
+        setVisible(true);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    public void showOnRandomPlace() {
+        Random random = new Random();
+        setLocation(random.nextInt(1200), random.nextInt(700));
+        getContentPane().setBackground(getColor());
+        repaint();
+    }
+
+    protected abstract Color getColor();
+}
