cc656a6d9cb8760df218a4d17c545dc915c5cc2d
AleksandraBogdan <aleksandrabogdan@mail.ru>
2019-11-12 21:10:25 +0300

    add property context

--- springLabTask/src/main/java/quoters/TerminatorQuoter.java
+++ springLabTask/src/main/java/quoters/TerminatorQuoter.java
@@ -10,6 +10,10 @@ public class TerminatorQuoter implements Quoter {
     @InjectRandomInt(min = 2, max = 7)
     private int repeat;
 
+    public void setRepeat(int repeat) {
+        this.repeat = repeat;
+    }
+
     @PostConstruct
     public void init() {
         System.out.println("Phase 2");
