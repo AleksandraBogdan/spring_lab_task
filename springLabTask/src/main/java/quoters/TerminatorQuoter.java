5b914e743face87d66cc1e79c2a5085edad95680
AleksandraBogdan <aleksandrabogdan@mail.ru>
2019-11-12 19:02:48 +0300

    add init method

--- springLabTask/src/main/java/quoters/TerminatorQuoter.java
+++ springLabTask/src/main/java/quoters/TerminatorQuoter.java
@@ -1,11 +1,22 @@
 package quoters;
 
+import javax.annotation.PostConstruct;
+
 public class TerminatorQuoter implements Quoter {
     private String message;
 
     @InjectRandomInt(min = 2, max = 7)
     private int repeat;
 
+    @PostConstruct
+    public void init() {
+        System.out.println("Phase 2");
+        System.out.println(repeat);
+    }
+
+    public TerminatorQuoter() {
+        System.out.println("Phase 1");
+    }
 
     public void setMessage(String message) {
         this.message = message;
